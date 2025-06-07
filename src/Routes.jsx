3a8fb718699629d24601652a3dd5b02af93e2624
marelhott <157871520+marelhott@users.noreply.github.com>
2025-06-07 07:24:57 +0200

    Add files via upload

---
+++ src/Routes.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
+import ScrollToTop from "components/ScrollToTop";
+import ErrorBoundary from "components/ErrorBoundary";
+import CostEstimationForm from "pages/cost-estimation-form";
+import NotFound from "pages/NotFound";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <ErrorBoundary>
+        <ScrollToTop />
+        <RouterRoutes>
+          <Route path="/" element={<CostEstimationForm />} />
+          <Route path="/cost-estimation-form" element={<CostEstimationForm />} />
+          <Route path="*" element={<NotFound />} />
+        </RouterRoutes>
+      </ErrorBoundary>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
